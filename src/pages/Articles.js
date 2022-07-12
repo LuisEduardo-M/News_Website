5337c04e944aa85a711f288d0a1e6481a484a883
Luis Eduardo M <luiseduardomc019@gmail.com>
2022-07-12 14:40:17 -0300

    api key

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -4,10 +4,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./../styles/Articles.css";
 
 const Articles = () => {
+    const apiKey = process.env.REACT_APP_API_KEY;
     const [news, setNews] = useState([]);
 
     useEffect(() => {
-        const url = "https://newsapi.org/v2/everything?q=Apple&from=2022-07-11&sortBy=popularity&apiKey=50f55c77a2b4476484c231bf485017db"
+        const url = `https://newsapi.org/v2/everything?q=Apple&from=2022-07-11&sortBy=popularity&apiKey=${apiKey}`;
 
         fetch(url)
             .then(res => res.json())
@@ -15,7 +16,9 @@ const Articles = () => {
                 console.log(data);
                 setNews(data.articles);
             });
-    }, []);
+
+    }, [apiKey]);
+
     return (
         <div>
             <h1 className="display-4">Articles Page</h1>
@@ -26,7 +29,7 @@ const Articles = () => {
                             {/* <span>{item.author}</span> */}
                             <h2 className="title">{item.title}</h2>
                             <p className="description">{item.description}</p>
-                            <img src={item.urlToImage} alt={item.title} /> <br/>
+                            <img src={item.urlToImage} alt={item.title} /> <br />
                             <a href={item.url} className="button">View</a> <hr />
                         </article>
                     );
